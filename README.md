b5f33a5499d631c63aad91be86eb7cab5af788bd
somya <sam69456@gmail.com>
2023-09-22 00:04:08 +0530

    update latest code

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # paytm-api-testing
+
+
+# i used  paytmdata.json file assets folder to get data beacuse api not working for local environment access block for cross origin for local server thats why i'm using i also integrate api provide me from your end also bind data 
--- src/app/pages/homelayout/homelayout.component.ts
+++ src/app/pages/homelayout/homelayout.component.ts
@@ -7,16 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./homelayout.component.scss']
 })
 export class HomelayoutComponent implements OnInit {
-pageCount:number = 10; // for using showing data per page 
+pageCount:number = 30; // for using showing data per page 
 itemPerPage:number = 1; // for using page data start with number
 categoryId:any = 9632;
 paytmData:any = [];
+products:any = [];
   constructor(public http:HttpClient) { }
 
   ngOnInit(): void {
 this.getPaytmData()  // api data load while page load
+
+// this function for using get data from local file assets
+this.http.get("assets/paytmdata.json").subscribe(data => {
+    this.products = data;
+})
   }
-  // using function for get data from paytm api 
+  // using function for get data from paytm api from server side
 getPaytmData(){
  let url = `https://search.paytm.com/v2/search?page_count=${this.itemPerPage}&items_per_page=${this.pageCount}&category=${this.categoryId}`
   this.http.get(url).subscribe((res)=>{
